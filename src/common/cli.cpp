f42a896987d8818d16aedc024b3d45271df1a65c
LeMoonStar <webmaster@unitcore.de>
2020-12-02 20:46:16 +0100

    added CLI class

---
+++ src/common/cli.cpp
@@ -0,0 +1,125 @@
+#include "cli.h"
+
+#include <iostream>
+#include "common.h"
+
+cli::cli(int width) : width(width), cursorPos(0) {
+}
+
+void cli::print(const std::string& str) {
+    // split string at newlines and loop trough lines
+    std::vector<std::string> lines = split(str);
+    for (auto line = lines.begin(); line != lines.end(); ++line) {
+        if (cursorPos == 0) {
+            std::cout << "# ";
+            cursorPos = 2;
+        }
+
+        // check if string fits into the box
+        if (line->size()+cursorPos < width) {
+            std::cout << *line;
+            cursorPos += line->size();
+            // fill width with spaces and close box with a #
+            while (cursorPos <= width-1) {
+                std::cout << " ";
+                ++cursorPos;
+            }
+            std::cout << "#" << std::endl;
+
+            cursorPos = 0;
+        } else {
+            // split string into multiple lines
+            size_t pos;
+            if ((pos = line->rfind(" ", width-cursorPos)) != std::string::npos) {
+                // could split string at the end of a word
+                print(line->substr(0, pos));
+                print(line->substr(pos+1, line->size()));
+            } else {
+                // need to split string in the middle of a word
+                print(line->substr(0, width-cursorPos-2) + "-");
+                print(line->substr(width-4, line->size()));
+            }
+        }
+
+    }
+}
+
+
+void cli::printList(std::vector<std::string> list) {
+    // get size of longest string
+    int size_per_item = 0;
+    for (auto it = list.begin(); it != list.end(); ++it) {
+        if (size_per_item < it->size())
+            size_per_item = it->size();
+    }
+
+    size_per_item += 1;
+
+    for (auto it = list.begin(); it != list.end(); ++it) {
+        if (cursorPos == 0) {
+            std::cout << "# ";
+            cursorPos = 2;
+        }
+        if (cursorPos + size_per_item < width) {
+            while (it->size() < size_per_item)
+                (*it) += ' ';
+            std::cout << *it;
+            cursorPos += size_per_item;
+            
+        } else {
+            // fill width with spaces and close box with a #
+            while (cursorPos <= width-1) {
+                std::cout << " ";
+                ++cursorPos;
+            }
+            std::cout << "#" << std::endl;
+
+            cursorPos = 0;
+            --it;
+        }
+    }
+    while (cursorPos <= width-1) {
+                std::cout << " ";
+                ++cursorPos;
+            }
+            std::cout << "#" << std::endl;
+
+            cursorPos = 0;
+}
+
+std::string cli::getInput(const std::string& desc) {
+    print(desc);
+    print("press # and Enter to confirm the input");
+    printLine();
+    std::cout << ">";
+    std::string input;
+    char ch;
+    while((ch = getchar()) != EOF) {
+        if (ch == '#')
+            break;
+
+        input += ch;
+    }
+    printLine();
+    return input;
+}
+
+std::string cli::getSingleLineInput(const std::string& desc) {
+    print(desc);
+    print("Enter ONE line, confirm with Enter");
+    printLine();
+    std::cout << ">";
+    std::string input;
+    std::cin >> input;
+    printLine();
+    return input;
+}
+
+void cli::printLine() {
+    while (cursorPos <= width) {
+        std::cout << "#";
+        ++cursorPos;
+    }
+    std::cout << std::endl;
+    cursorPos = 0;
+}
---
+++ src/common/cli.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+class cli {
+    int width;
+    int cursorPos;
+public:
+    /**
+     * @brief Construct a new cli object
+     * 
+     * @param width 
+     */
+    cli(int width = 65);
+
+    /**
+     * @brief a function to print text inside the cli box
+     * 
+     * @param str the string to be printed
+     */
+    void print(const std::string& str);
+    /**
+     * @brief a function to print nicely formatted lists
+     * 
+     * @param list the list to be printed
+     */
+    void printList(std::vector<std::string> list);
+
+    /**
+     * @brief get multiline Input from the user
+     * 
+     * @param desc a description to be shown to the user
+     * @return std::string the text inputted by the user
+     */
+    std::string getInput(const std::string& desc);
+    /**
+     * @brief get a single line of input from  the user
+     * 
+     * @param desc a description to be shown to the user
+     * @return std::string the text inputted by the user
+     */
+    std::string getSingleLineInput(const std::string& desc);
+
+    /**
+     * @brief draw a seperator line
+     * 
+     */
+    void printLine();
+};
