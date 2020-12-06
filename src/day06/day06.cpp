6e80242295c756826a3a43f4526782abc6293f3c
LeMoonStar <webmaster@unitcore.de>
2020-12-06 11:33:51 +0100

    added day06 part 1

--- src/day06/day06.cpp
+++ src/day06/day06.cpp
@@ -3,6 +3,44 @@
 #include <common.h>
 #include <cli.h>
 
-void day06(cli& c) {
+class Group {
+    std::vector<char> data;
+public:
+    Group(const std::string& str) {
+        data = splitToChars(str);
+    }
 
+    int count() {
+        int count = 0;
+        std::map<char, bool> found_characters;
+        for (auto it = data.begin(); it != data.end(); ++it) {
+            if (found_characters.find(*it) == found_characters.end()) {
+                found_characters[*it] = true;
+                count++;
+            }
+        }
+        return count;
+    }
+};
+
+void day06(cli& c) {
+    std::vector<std::string> data = c.getSplittedInput("please enter your input for Day 6 (https://adventofcode.com/2020/day/6)");
+
+    std::vector<Group> groups;
+    std::string group_input;
+    for (auto it = data.begin(); it != data.end(); ++it) {
+        if (*it == "") {
+            groups.push_back(group_input);
+            group_input = "";
+            continue;
+        }
+        group_input += *it;
+    }
+
+    int count = 0;
+    for (auto it = groups.begin(); it != groups.end(); ++it) {
+        count += it->count();
+    }
+
+    c.print("the solution to part 1 is: " + std::to_string(count));
 }
