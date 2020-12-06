22a42da71e29f9c28d5d085ddab1a05d9f587099
LeMoonStar <webmaster@unitcore.de>
2020-12-06 12:49:19 +0100

    added day06 part 2

--- src/day06/day06.cpp
+++ src/day06/day06.cpp
@@ -4,21 +4,39 @@
 #include <cli.h>
 
 class Group {
-    std::vector<char> data;
+    const std::vector<std::string> data;
 public:
-    Group(const std::string& str) {
-        data = splitToChars(str);
+    Group(const std::string& str) : data(split(str)) {
     }
 
-    int count() {
+    int countPart1() {
         int count = 0;
         std::map<char, bool> found_characters;
-        for (auto it = data.begin(); it != data.end(); ++it) {
-            if (found_characters.find(*it) == found_characters.end()) {
-                found_characters[*it] = true;
-                count++;
+        for (auto it = data.begin(); it != data.end(); ++it)
+            for (auto it2 = it->begin(); it2 != it->end(); ++it2) {
+                if (found_characters.find(*it2) == found_characters.end()) {
+                    found_characters[*it2] = true;
+                    count++;
+                }
             }
+        return count;
+    }
+
+    int countPart2() {
+        int count = 0;
+        int person_count = data.size()-1;
+        std::map<char, int> found_characters;
+
+        for (auto it = data.begin(); it != data.end(); ++it)
+            for (auto it2 = it->begin(); it2 != it->end(); ++it2) {
+                if (*it2 != '\n' && *it2 != '\r')
+                    found_characters[*it2]++;
+            }
+        for (auto it = found_characters.begin(); it != found_characters.end(); ++it) {
+            if (it->second == person_count)
+                count++;
         }
+
         return count;
     }
 };
@@ -34,13 +52,20 @@ void day06(cli& c) {
             group_input = "";
             continue;
         }
-        group_input += *it;
+        group_input +=  *it + "\n";
     }
 
     int count = 0;
     for (auto it = groups.begin(); it != groups.end(); ++it) {
-        count += it->count();
+        count += it->countPart1();
     }
 
     c.print("the solution to part 1 is: " + std::to_string(count));
+
+    count = 0;
+    for (auto it = groups.begin(); it != groups.end(); ++it) {
+        count += it->countPart2();
+    }
+
+    c.print("the solution to part 2 is: " + std::to_string(count));
 }
