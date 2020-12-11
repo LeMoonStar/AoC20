737c67d0760aa02e1126e990cc13629b2ffdc918
LeMoonStar <webmaster@unitcore.de>
2020-12-11 20:29:39 +0100

    added day11 part 2

--- src/day11/day11.cpp
+++ src/day11/day11.cpp
@@ -15,21 +15,21 @@ enum Spot {
 
 // Thanks https://stackoverflow.com/questions/8473009/how-to-efficiently-compare-two-maps-of-strings-in-c-only-for-a-subset-of-the-k
 template <typename Map>
-bool map_compare(Map const &lhs, Map const &rhs) {
+static bool map_compare(Map const &lhs, Map const &rhs) {
     // No predicate needed because there is operator== for pairs already.
     return lhs.size() == rhs.size()
         && std::equal(lhs.begin(), lhs.end(),
                       rhs.begin());
 }
 
-inline Spot getSpotFromChar(char c) {
+static inline Spot getSpotFromChar(char c) {
     if (c == '.') return Spot::FLOOR;
     if (c == 'L') return Spot::SEAT;
     if (c == '#') return Spot::OCCUPIED;
     return Spot::UNKNOWN;
 }
 
-inline char getCharFromSpot(Spot s) {
+static inline char getCharFromSpot(Spot s) {
     if (s == Spot::FLOOR) return '.';
     if (s == Spot::SEAT) return 'L';
     if (s == Spot::OCCUPIED) return '#';
@@ -37,7 +37,7 @@ inline char getCharFromSpot(Spot s) {
 }
 
 template <typename Map, typename T>
-inline int count_occurences(const Map& m, const T& v) {
+static inline int count_occurences(const Map& m, const T& v) {
     int i = 0;
     for (auto it = m.begin(); it != m.end(); ++it) {
         if (it->second == v) ++i;
@@ -45,7 +45,7 @@ inline int count_occurences(const Map& m, const T& v) {
     return i;
 }
 
-std::map<std::pair<int, int>, Spot> evolve(std::map<std::pair<int, int>, Spot>& spots, int width, int height) {
+static std::map<std::pair<int, int>, Spot> evolvePart1(std::map<std::pair<int, int>, Spot>& spots, int width, int height) {
     std::map<std::pair<int, int>, int> neighbours;
     for (int y = 0; y < height; ++y) {
         for (int x = 0; x < width; ++x) {
@@ -68,12 +68,61 @@ std::map<std::pair<int, int>, Spot> evolve(std::map<std::pair<int, int>, Spot>& 
     for (int y = 0; y < height; ++y) {
         for (int x = 0; x < width; ++x) {
             if (spots[std::make_pair(x,y)] == Spot::OCCUPIED && neighbours[std::make_pair(x,y)] >= 4) {
-                //cl->print("AAAAAAA");
                 evolved[std::make_pair(x,y)] = Spot::SEAT;
                 continue;
             }
             if (spots[std::make_pair(x,y)] == Spot::SEAT && neighbours[std::make_pair(x,y)] == 0) {
-                //cl->print("BBBBBBB");
+                evolved[std::make_pair(x,y)] = Spot::OCCUPIED;
+                continue;
+            }
+        }
+    }
+
+    return evolved;
+
+};
+
+static inline bool lookInDirection(std::map<std::pair<int, int>, Spot>& spots, int width, int height, int x, int y, int dirx, int diry) {
+    while (true) {
+        x += dirx;
+        y += diry;
+
+        if (x < 0 || x >= width) break;
+        if (y < 0 || y >= height) break;
+
+        if (spots[std::make_pair(x,y)] == Spot::OCCUPIED) return true;
+        if (spots[std::make_pair(x,y)] == Spot::SEAT) return false;
+    }
+    return false;
+}
+
+static std::map<std::pair<int, int>, Spot> evolvePart2(std::map<std::pair<int, int>, Spot>& spots, int width, int height) {
+    std::map<std::pair<int, int>, int> neighbours;
+    for (int y = 0; y < height; ++y) {
+        for (int x = 0; x < width; ++x) {
+            int i = 0;
+            if (lookInDirection(spots, width, height, x, y, 1, 0)) ++i;
+            if (lookInDirection(spots, width, height, x, y, 0, 1)) ++i;
+            if (lookInDirection(spots, width, height, x, y, 1, 1)) ++i;
+            if (lookInDirection(spots, width, height, x, y, -1, 0)) ++i;
+            if (lookInDirection(spots, width, height, x, y, 0, -1)) ++i;
+            if (lookInDirection(spots, width, height, x, y, -1, -1)) ++i;
+            if (lookInDirection(spots, width, height, x, y, 1, -1)) ++i;
+            if (lookInDirection(spots, width, height, x, y, -1, 1)) ++i;
+
+            neighbours[std::make_pair(x, y)] = i;
+        }
+    }
+
+    std::map<std::pair<int, int>, Spot> evolved = spots;
+    
+    for (int y = 0; y < height; ++y) {
+        for (int x = 0; x < width; ++x) {
+            if (spots[std::make_pair(x,y)] == Spot::OCCUPIED && neighbours[std::make_pair(x,y)] >= 5) {
+                evolved[std::make_pair(x,y)] = Spot::SEAT;
+                continue;
+            }
+            if (spots[std::make_pair(x,y)] == Spot::SEAT && neighbours[std::make_pair(x,y)] == 0) {
                 evolved[std::make_pair(x,y)] = Spot::OCCUPIED;
                 continue;
             }
@@ -88,34 +137,38 @@ void day11(cli& c) {
     cl = &c;
     std::vector<std::string> input = c.getSplittedInput("please enter your input for Day 11 (https://adventofcode.com/2020/day/11)");
 
-    std::map<std::pair<int, int>, Spot> spots;
+    std::map<std::pair<int, int>, Spot> originalSpots;
     int height = 0, width = 0;
     for (auto it = input.begin(); it != input.end(); ++it) {
         if (*it == "") continue;
         width = 0;
         for (auto c = it->begin(); c != it->end(); ++c) {
-            spots[std::make_pair(width, height)] = getSpotFromChar(*c);
+            originalSpots[std::make_pair(width, height)] = getSpotFromChar(*c);
             ++width;
         }
         ++height;
     }
 
+    std::map<std::pair<int, int>, Spot> spots = originalSpots;
     while (true) {
-        auto newSpots = evolve(spots, width, height);
+        auto newSpots = evolvePart1(spots, width, height);
         if (map_compare(newSpots, spots)) {
             break;
         }
         spots = newSpots;
     }
 
-    /*for (int y = 0; y < height; y++) {
-        std::string line = "";
-        for (int x = 0; x < width; x++) {
-            line += getCharFromSpot(spots[std::make_pair(x,y)]);
-        }
-        c.print(line);
-    }
-    c.printLine();*/
     c.print("the solution to part 1 is: " + std::to_string(count_occurences(spots, Spot::OCCUPIED)));
+
+    spots = originalSpots;
+    while (true) {
+        auto newSpots = evolvePart2(spots, width, height);
+        if (map_compare(newSpots, spots)) {
+            break;
+        }
+        spots = newSpots;
+    }
+
+    c.print("the solution to part 2 is: " + std::to_string(count_occurences(spots, Spot::OCCUPIED)));
     
 }
